5d6d606f50c6017f071dc1d82eea8360254ebe15
nozila2024 <nozilarajabboyeva@gmail.com>
2025-11-13 17:31:26 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+#1-misol
+n = "Nozila"
+b = "Rajabboyeva"
+i = n + " " + b
+print(i.upper())
+
+#2-misol
+n = "Nozila"
+b = "Rajabboyeva"
+i = n + " " + b
+print(i.lower())
+
+#3-misol
+n = "nozila rajabboyeva"
+print(n.title())
+
+#4-misol
+n = "Nozila Rajabboyeva"
+print(n.capitalize())
+
+#5-misol
+n = "  Nozila  "
+print(n.strip())
+
+#6-misol
+n = "  Nozila  "
+print(n.lstrip())
+
+#7-misol
+n = "  Nozila  "
+print(n.rstrip())
+
+#8-misol
+n = input("Ismingiz nima: ")
+print(f"Privet {n}")
